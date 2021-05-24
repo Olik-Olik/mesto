8387823af2c6f65b8a99efb4e771dbd27bbae5a3
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-05-24 22:55:48 +0300

    Header is great.Perfect Pixel.

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,24 @@
 let container = document.querySelector('.container');
 
-let heartButton = document.querySelector('.elements__heart');
-heartButton.addEventListener('click', () =>
-    heartButton.classList.toggle('elements__heart-active'));//переключит на черное хотелось бы
+//let heartButton = document.querySelector('.elements__heart');
+//heartButton.addEventListener('click', () =>
+//heartButton.classList.toggle('elements__heart-active'));//переключит на черное хотелось бы
+
+//let editButton = document.querySelector('.profile__edit-button');
+//let popupClose = container.querySelector('.popup__close');
+//let popup = container.querySelector('.popup');
+//editButton.addEventListener('click', toggleClass);
+//popupClose.addEventListener('click', toggleClass);
+
 
 let editButton = document.querySelector('.profile__edit-button');
-let popupClose = container.querySelector('.popup__close');
 let popup = container.querySelector('.popup');
-editButton.addEventListener('click', toggleClass);
-popupClose.addEventListener('click', toggleClass);
+editButton.addEventListener('click',function (){
+popup.classList.add('popup_visible');})
+
+let popupClose = container.querySelector('.popup__close');
+popupClose.addEventListener('click',function (){
+popup.classList.remove ('popup_visible');})
 
 function toggleClass() {
     popup.classList.toggle('popup_visible');
@@ -24,15 +34,15 @@ let jobInput = container.querySelector('.popup__field_job');
 function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Получите значение полей jobInput и nameInput из свойства value
-    jobInput.getAttribute('value:');
-    nameInput.getAttribute('value');
+   // jobInput.getAttribute('value:');
+   // nameInput.getAttribute('value');
 
     // Выберите элементы, куда должны быть вставлены значения полей
-    let ProfileTitle = container.querySelector('.profile__title');
-    let ProfileSubtitle = container.querySelector('.profile__subtitle');
+    let profileTitle = container.querySelector('.profile__title');
+    let profileSubtitle = container.querySelector('.profile__subtitle');
     // Вставьте новые значения с помощью textContent
-    ProfileTitle.textContent = nameInput.value;
-    ProfileSubtitle.textContent = jobInput.value;
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
     toggleClass();
 }
 
