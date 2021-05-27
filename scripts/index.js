922d3a18436b9f3cc877999387b9a02c1a51bd2e
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-05-27 18:27:02 +0300

    Close outside

--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,39 @@
-/*
-//обьявляем переменные
-let popUpSave = document.querySelector('.popup__save');
-*/
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-editButton.addEventListener('click',function (){
-popup.classList.add('popup_opened');})
-
+let savePopup = document.getElementById('popup__field');
+let closePopup = document.querySelector('.popup__close-button');
+let popupName = document.getElementById('popup__field_name');
+let popupJob = document.getElementById('popup__field_job');
+//куда это будет вставлено
 let nameInput = document.querySelector('.profile__title');
-let popupName = document.getElementsByid('popup__name');
 let jobInput = document.querySelector('.profile__subtitle');
-let popupJob = document.getElementsByid('popup__job');
 
- popupName.value = nameInput.textContent;
- popupJob.value = jobInput.textContent;
+/*editButton.addEventListener('click', function () {
+    popup.classList.add('popup_opened');
+    popupName.value = nameInput.textContent;
+    popupJob.value = jobInput.textContent;
+})*/
 
-//удаляем=закрываем
-let popupClose = document.querySelector('.popup__close-button');
-    popupClose.addEventListener('click',function (){
-    popup.classList.remove('popup_opened');})
-
-
-/*
-//теперь функции open
-/!*function editButton() {
+function workPopup() {
     popupName.value = nameInput.textContent;
     popupJob.value = jobInput.textContent;
     popup.classList.add('popup_opened');
-}*!/
-
-popupOpen.addEventListener('click',function (){
-/!* popupName.value = nameInput.textContent;
- popupJob.value = jobInput.textContent; *!/
-popup.classList.add('popup_opened');})
-})
-
-//функция сабмита
-// Находим форму в DOM  formSubmitHandler
-//Слушаем в submitPopup содержимое класса формы
-formElement
-let submitPopup = container.querySelector('.container')
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileTitle = container.querySelector('.profile__title');
-    let profileSubtitle = container.querySelector('.profile__subtitle');
-    // Вставьте новые значения с помощью textContent
-    profileTitle.textContent = nameInput.value;
-    profileSubtitle.textContent = jobInput.value;
-    toggleClass();
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
 
+function closePopupAll() {
+    popup.classList.remove('popup_opened');
+}
 
+/*popupClose.addEventListener('click', function () {
+    popup.classList.remove('popup_opened');
+})*/
+function SubmitHandler(evt) {
+    evt.preventDefault();
+     popupName.textContent =nameInput.value;
+     popupJob.textContent = jobInput.value;
+    closePopupAll();
+}
 
-*/
+editButton.addEventListener('click', workPopup);
+closePopup.addEventListener('click', closePopupAll);
+savePopup.addEventListener('submit', SubmitHandler);
