a30a99ac2f1a565ccc8498ca8c901a5bde6f3dc6
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-06-29 01:30:35 +0300

    Validate new Card is working

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
 //Переменные перенесены в constants.js
 
-import { keyCodeEsc, editButton,popupPlace,submitPopupPlaceButton,closePopupPlaceButton,inputListpopupPlace,
-    popupChangeProfile,closePopupChangeProfileButton,imagePopupCloseButton,openPopupPlaceButton,formEditProfile,
-    formAddCard,inputUserName,inputUserJob,inputCardName,inputCardLink,nameProfileElement,jobProfileElement,
-    cardsList,zoomedImage,imageDescription,itemTemplate,initialCards} from '../variables/constants.js';
+import {
+    keyCodeEsc, editButton, popupPlace, submitPopupPlaceButton, closePopupPlaceButton, inputListpopupPlace,
+    popupChangeProfile, closePopupChangeProfileButton, imagePopupCloseButton, openPopupPlaceButton, formEditProfile,
+    formAddCard, inputUserName, inputUserJob, inputCardName, inputCardLink, nameProfileElement, jobProfileElement,
+    cardsList, zoomedImage, imageDescription, itemTemplate, initialCards
+} from '../variables/constants.js';
 
-import  {Card}  from './Card.js'
+import {Card} from './Card.js'
 import {FormValidator} from './FormValidator.js';
 import {configs} from '../variables/configs.js';
 
@@ -53,67 +55,71 @@ function handleImageView(evt) {//обработчик события
     imageDescription.textContent = evt.currentTarget.closest("#template-id").querySelector('.elements__word').textContent;
 }
 
-    function openEditProfilePopup() {
-        //открытие попапа с редактированием профиля
-        inputUserName.value = nameProfileElement.textContent;
-        inputUserJob.value = jobProfileElement.textContent;
-        openPopup(popupChangeProfile);
-    }
+function openEditProfilePopup() {
+    //открытие попапа с редактированием профиля
+    inputUserName.value = nameProfileElement.textContent;
+    inputUserJob.value = jobProfileElement.textContent;
+    openPopup(popupChangeProfile);
+}
 
-    function openAddCardPopup() {
-        //открытие попапа с местом
-        inputListpopupPlace.forEach((inputElement) => {
-            inputElement.value = '';
-        })
-       // disableButton(submitPopupPlaceButton); подумать позднее
-        openPopup(popupPlace);
-    }
+function openAddCardPopup() {
+    //открытие попапа с местом
+    inputListpopupPlace.forEach((inputElement) => {
+        inputElement.value = '';
+    })
+    //disableButton(submitPopupPlaceButton); //подумать позднее
+    openPopup(popupPlace);
+}
 
-    function submitAddCardPopup(evt) {
-        // сохраняем введенные значения 2 popup
-        evt.preventDefault();
-        const newArrayElement = {
-            name: inputCardName.value,
-            link: inputCardLink.value
-        };
-        const newCard = createCard(newArrayElement);
-        cardsList.prepend(newCard);
-        formAddCard.reset();
-        closePopup(evt);
-    }
 //последняя итерация -кладем в ДОМ 1.5
 function renderAllCards() {
     initialCards.forEach(function (item) {
-        const card = new Card(item);
+        const card = new Card();
         const newCard = card._createCard(item);
         cardsList.append(newCard);
         //кладем в ДОМ
     });
 }
-    editButton.addEventListener('click', openEditProfilePopup);
-    openPopupPlaceButton.addEventListener('click', openAddCardPopup);
-    closePopupPlaceButton.addEventListener('click', closePopup);
-    closePopupChangeProfileButton.addEventListener('click', closePopup);
-    formEditProfile.addEventListener('submit', submitHandlerProfile);
-    formAddCard.addEventListener('submit', submitAddCardPopup);
-    imagePopupCloseButton.addEventListener('click', closePopup);
-    window.addEventListener("load", renderAllCards);
 
+function submitAddCardPopup(evt) {
+    // сохраняем введенные значения 2 popup
+    evt.preventDefault();
+    const newArrayElement =
+        {
+        name: inputCardName.value,
+        link: inputCardLink.value
+    };
+    const card = new Card();
+    const newCard = card._createCard(newArrayElement);
+    cardsList.prepend(newCard);
+    formAddCard.reset();
+    closePopup(evt);
+}
+
+
+editButton.addEventListener('click', openEditProfilePopup);
+openPopupPlaceButton.addEventListener('click', openAddCardPopup);
+closePopupPlaceButton.addEventListener('click', closePopup);
+closePopupChangeProfileButton.addEventListener('click', closePopup);
+formEditProfile.addEventListener('submit', submitHandlerProfile);
+formAddCard.addEventListener('submit', submitAddCardPopup);
+imagePopupCloseButton.addEventListener('click', closePopup);
+window.addEventListener("load", renderAllCards);
 
 
 //ищем все оверлеи=блокеры,  навешиваем листенер на онклик
-    const blockerList = Array.from(document.querySelectorAll('.blocker'));
-    blockerList.forEach((blocker) => {
-        blocker.addEventListener('click', (evt) => {
-            closePopup(evt);
-        });
+const blockerList = Array.from(document.querySelectorAll('.blocker'));
+blockerList.forEach((blocker) => {
+    blocker.addEventListener('click', (evt) => {
+        closePopup(evt);
     });
+});
 
 
 /*configs.forEach(config =>{
      new FormValidator(config);
 });*/
-const formValidatorProfile= new FormValidator(configs[0]);
+const formValidatorProfile = new FormValidator(configs[0]);
 const formValidatorCard = new FormValidator(configs[1]);
 export {openPopup};
 
