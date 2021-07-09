71f0ad950755d721713963cd3444db89333fe07d
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-07-10 02:08:22 +0300

    in proc

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,9 @@
+const numbers = [2, 3, 5];
 
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
 import {
     keyCodeEsc,
     editButton,
